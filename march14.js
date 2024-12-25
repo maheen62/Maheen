ad89d58613304cd8a79f7c28d526e6c3fdbe0754
Museum of candy <dmaheen4037@gmail.com>
2024-12-25 21:59:38 +0530

    essageJavascriptPractice

---
+++ march14.js
@@ -0,0 +1,84 @@
+// OPERATORS //
+
+// Arthematic Operator = + , - ,  * ,/ , % //
+// Assignment Operator  =  = , += , -= 
+// Comparision Operator = <> , <= , >= , == , === , !=
+// Conditional Operator =  &&  ||
+// turnary Operator = ?=
+// logical Operator 
+// type Operator = Typeof and instanceof
+
+// EXPRESSION
+
+var  num1= 20; 
+var num2 = 30;
+var num3 = 40;
+var sum = num1 + num2 + num3
+console.log(sum)
+
+// DATA TYPE //
+
+// storing numbers //
+ var a = 10;
+ var b = a;
+ console.log(b)
+
+ // storing stirngs 
+var name = "rahul"
+console.log(name)
+
+// undefined 
+// variable is created but we didnot assign value to it
+var a;
+console.log(a)
+
+// not define
+// variable is not created
+console.log(a)
+
+// typeof 
+var a = "maheen";
+console.log(typeof(a));
+
+var b = 10;
+console.log(typeof(b));
+
+var c = true;
+console.log(typeof(c));
+
+// practicing numbers
+
+console.log(0/0)
+console.log(0/5)
+console.log(5/0)
+console.log(3 * "a")
+console.log('a' * 5)
+console.log('b' + 4)
+console.log(9 + 'e')
+
+// string pracctice
+var str = "i love 'javascript'"
+console.log(str)
+
+var str = 'i love "javascript"'
+console.log(str)
+
+var str = "i love \"javascript\""
+console.log(str)
+
+
+var str1 = "To Day"
+var str2 = "Is"
+var str3 = "Thursday"
+console.log(str1 + " " + str2 +  " " + str3 ) 
+
+var str1 = "To Day"
+var str2 = "Is"
+var str3 = "Thursday"
+console.log(str1.concat(str2 , str3))
+
+
+
+
+
+
